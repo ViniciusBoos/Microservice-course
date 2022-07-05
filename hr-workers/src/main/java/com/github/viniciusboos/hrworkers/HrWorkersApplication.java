658ef3a3ba31fda13160f434e6ee001138fdbe2c
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-05 16:00:40 -0400

    Primeiro commit com as bases do projeto

---
+++ hr-workers/src/main/java/com/github/viniciusboos/hrworkers/HrWorkersApplication.java
@@ -0,0 +1,13 @@
+package com.github.viniciusboos.hrworkers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkersApplication.class, args);
+	}
+
+}
