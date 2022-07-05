0cc6572f2dd891ed03045658076391fa183d5124
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-05 16:41:59 -0400

    Criando metodo get para encontrar por id como parametro de rota

--- hr-workers/src/main/java/com/github/viniciusboos/hrworkers/resources/WorkerResource.java
+++ hr-workers/src/main/java/com/github/viniciusboos/hrworkers/resources/WorkerResource.java
@@ -5,10 +5,12 @@ import com.github.viniciusboos.hrworkers.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/workers")
@@ -20,5 +22,11 @@ public class WorkerResource {
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
         return ResponseEntity.ok(workerRepository.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Optional<Worker> possibleWorker = workerRepository.findById(id);
+        return possibleWorker.map(ResponseEntity::ok).orElse(null);
     }
 }
