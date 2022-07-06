35d31bcd9c8a1cae1fefeb6747e849c32afc0036
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-06 14:07:54 -0400

    modificando para rodar utilizando apenas o nome dos outros microservisos, utilizando do eureka server]

--- hr-workers/src/main/java/com/github/viniciusboos/hrworkers/HrWorkersApplication.java
+++ hr-workers/src/main/java/com/github/viniciusboos/hrworkers/HrWorkersApplication.java
@@ -2,7 +2,9 @@ package com.github.viniciusboos.hrworkers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkersApplication {
 
