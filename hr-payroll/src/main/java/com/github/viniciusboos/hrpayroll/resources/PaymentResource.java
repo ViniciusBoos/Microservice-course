5fdeeea861da7a14611433fe78cca025504dfbed
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-06 16:27:12 -0400

    Adicionando configurações de timeout para o ribbon e hystrix

--- hr-payroll/src/main/java/com/github/viniciusboos/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/github/viniciusboos/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.github.viniciusboos.hrpayroll.resources;
 
 import com.github.viniciusboos.hrpayroll.entities.Payment;
 import com.github.viniciusboos.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +17,17 @@ public class PaymentResource {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId,
                                               @PathVariable Integer days) {
 
         Payment payment = paymentService.getPayment(workerId, days);
         return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative( Long workerId,
+                                               Integer days){
+        return ResponseEntity.notFound().build();
     }
 }
