747f02e802e86a452e6544cb7044ed4d9d995f11
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-06 13:27:11 -0400

    Modificando o projeto para rodar em duas portas para testar o hr-payment

--- hr-workers/src/main/java/com/github/viniciusboos/hrworkers/resources/WorkerResource.java
+++ hr-workers/src/main/java/com/github/viniciusboos/hrworkers/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.github.viniciusboos.hrworkers.resources;
 
 import com.github.viniciusboos.hrworkers.entities.Worker;
 import com.github.viniciusboos.hrworkers.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -26,6 +35,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("PORT = " + environment.getProperty("local.server.port"));
+
         Optional<Worker> possibleWorker = workerRepository.findById(id);
         return possibleWorker.map(ResponseEntity::ok).orElse(null);
     }
