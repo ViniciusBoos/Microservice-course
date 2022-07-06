fcb285023ad968de1a48fee7cc2ce32766c3ca99
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-06 09:09:05 -0400

    Criando feign config e atualizado payment service para usar feign

--- hr-payroll/src/main/java/com/github/viniciusboos/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/github/viniciusboos/hrpayroll/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.github.viniciusboos.hrpayroll.service;
 
 import com.github.viniciusboos.hrpayroll.entities.Payment;
 import com.github.viniciusboos.hrpayroll.entities.Worker;
+import com.github.viniciusboos.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,19 +14,12 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(Long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-
-        uriVariables.put("id", "" + workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
 
         return new Payment(worker.getNome(), worker.getDailyIncome(), days);
     }
