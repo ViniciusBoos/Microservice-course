3777bc3114fb3933569e28357d357b4c1f2bf7f4
Vinicius Vieira Boos <vinicius.boos@squadra.com.br>
2022-07-08 09:14:09 -0400

    Configurando zuul e hr-oauth para pegar as configurações no repositorio git privado através do config server

--- hroauth/src/main/java/com/github/viniciusboos/hroauth/config/AppConfig.java
+++ hroauth/src/main/java/com/github/viniciusboos/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.github.viniciusboos.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,10 +15,13 @@ public class AppConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
--- hroauth/src/main/java/com/github/viniciusboos/hroauth/config/AuthorizationServerConfig.java
+++ hroauth/src/main/java/com/github/viniciusboos/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,8 @@
 package com.github.viniciusboos.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,7 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-
+@RefreshScope
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -28,6 +30,12 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("${oauth.client.name}")
+    private String clientName;
+
+    @Value("${oauth.client.secret}")
+    private String clientSecret;
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -36,8 +44,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("myappname123")
-                .secret(passwordEncoder.encode("myappsecret123"))
+                .withClient(clientName)
+                .secret(passwordEncoder.encode(clientSecret))
                 .scopes("read","write")
                 .authorizedGrantTypes("password").accessTokenValiditySeconds(86400);
     }
